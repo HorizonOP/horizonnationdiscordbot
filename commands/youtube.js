7dcfd6542b27a772f22bde88de358a3d740a2920
HorizonOP <79844761+HorizonOP@users.noreply.github.com>
2021-04-18 07:35:24 +0400

    Update youtube.js

--- commands/youtube.js
+++ commands/youtube.js
@@ -1,7 +1,28 @@
-module.exports = {
+module.exports = 
+{
     name: 'youtube',
-    description: "This is a youtube command",
-    execute(message, args){
-        message.channel.send('https://www.youtube.com/channel/UCJtbyCHmt_TAa9RxgC2aA8w')
+    aliases: ['yt', 'utube'],
+    cooldown: 0,
+    usage: `.youtube`,
+    description: "this is a link for youtube",
+    catergory: 'other',
+    permissions: [],
+    execute(message, args, cmd, client, Discord)
+    {
+        const youtubeEmbed = new Discord.MessageEmbed()
+        .setColor('#95ff00')
+        .setTitle('Youtube Link')
+        .setAuthor('Horizon')
+        .setDescription('Youtube is Great Place')
+        .addFields
+        (
+        {name: '\u200B', value: '\u200B'},
+        {name: 'Youtube', value: 'https://www.youtube.com/channel/UCJtbyCHmt_TAa9RxgC2aA8w'},
+        )
+        .setTimestamp()
+        .setFooter('Yeetus Feetus.');
+
+        message.channel.send(youtubeEmbed)
     }
 }
+
