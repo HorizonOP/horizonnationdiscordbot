efdf0ae7001b29a28a65e2f2f8ab2088c8220c38
HorizonOP <79844761+HorizonOP@users.noreply.github.com>
2021-04-18 08:35:44 +0400

    lmao

--- commands/youtube.js
+++ commands/youtube.js
@@ -1,29 +1,7 @@
-const Discord = require('discord.js')
-module.exports = 
-{
+module.exports = {
     name: 'youtube',
-    aliases: ['yt', 'utube'],
-    cooldown: 0,
-    usage: `.youtube`,
-    description: "this is a link for youtube",
-    catergory: 'other',
-    permissions: [],
-    execute(message, args, cmd, client, Discord)
-    {
-        const youtubeEmbed = new Discord.MessageEmbed()
-        .setColor('#95ff00')
-        .setTitle('Youtube Link')
-        .setAuthor('Horizon')
-        .setDescription('Youtube is Great Place')
-        .addFields
-        (
-        {name: '\u200B', value: '\u200B'},
-        {name: 'Youtube', value: 'https://www.youtube.com/channel/UCJtbyCHmt_TAa9RxgC2aA8w'},
-        )
-        .setTimestamp()
-        .setFooter('Yeetus Feetus.');
-
-        message.channel.send(youtubeEmbed)
+    description: "This is a youtube command",
+    execute(message, args){
+        message.channel.send('https://www.youtube.com/channel/UCJtbyCHmt_TAa9RxgC2aA8w')
     }
 }
-
