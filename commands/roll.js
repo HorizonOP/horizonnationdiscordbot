06a348207c36c77617c3310ffd90b41af4fcb600
HorizonOP <79844761+HorizonOP@users.noreply.github.com>
2021-04-22 10:33:44 +0400

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'youtube',
+    name: 'roll',
     description: "This is a youtube command",
     execute(message, args){
         message.channel.send('https://tenor.com/view/dance-moves-dancing-singer-groovy-gif-17029825')
