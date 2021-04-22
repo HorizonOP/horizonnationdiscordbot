099c2d838812fac7eadbeb2b636939eef1277527
HorizonOP <79844761+HorizonOP@users.noreply.github.com>
2021-04-22 10:22:06 +0400

    Delete roll.js

--- commands/roll.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'roll',
-    description: "This is a rickroll  command",
-    execute(message, args){
-        message.channel.send('https://media.discordapp.net/attachments/679450620079636491/799143607856267264/image0.gif')
-    }
-}
