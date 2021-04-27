8a4b8029db6d5366ee96adc1b687fe7fbabb7829
HorizonOP <79844761+HorizonOP@users.noreply.github.com>
2021-04-27 11:17:38 +0400

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -1,15 +1,16 @@
 module.exports = {
     name: 'status',
     description: "Ping pong command!",
-    execute(message, args, client){
-        if(!message.author.id === '640052930682159112') return message.channel.send('Noob you aint owner of this bot')
+    execute(message, args, client) {
+        let type = args[0]
+        let status = args.slice(1).join(" ")
+        if (!args[0]) return message.channel.send("Correct usage: +status <type> <status>")
 
-        
-  let type = args[0]
-  let status = args.slice(1).join(" ")
-  if(!args[0]) return message.channel.send("Correct usage: +status <type> <status>")
-
-  client.user.setActivity(`${status}`,{ type: `${type}` })
-  message.channel.send(`Successfully changed my status to ${type}: "${status}"`)
+        if (message.author.id === '640052930682159112') {
+            client.user.setActivity(`${status}`, { type: `${type}` })
+            message.channel.send(`Successfully changed my status to ${type}: "${status}"`)
+        } else {
+            message.channel.send('Noob you aint owner of this bot')
+        }
     }
 }
