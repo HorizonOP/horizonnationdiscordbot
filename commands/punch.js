f19bef7e9e3a35b59617faf1c634ad5a8d4dbb66
HorizonOP <79844761+HorizonOP@users.noreply.github.com>
2021-04-27 09:56:19 +0400

    Update punch.js

--- commands/punch.js
+++ commands/punch.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'punch',
     description: "Ping pong command!",
     execute(message, args){
-        message.channel.send('You gave him an RKO in the face and knocked him out. He was crying like a baby hahahahah https://giphy.com/gifs/rko-yQlNXGTprtGsE')
+        message.channel.send(' https://giphy.com/gifs/rko-yQlNXGTprtGsE You gave him an RKO in the face and knocked him out. He was crying like a baby hahahahah ')
     }
 }
