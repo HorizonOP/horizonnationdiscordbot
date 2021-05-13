a1092906f86ac7c41790d51bec8b31fb03c47bdd
HorizonOP <79844761+HorizonOP@users.noreply.github.com>
2021-05-13 10:29:33 +0400

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -8,7 +8,7 @@ module.exports = {
         .setURL('https://www.youtube.com/channel/UCJtbyCHmt_TAa9RxgC2aA8w')
         .setDescription('Invite the bot to your server')
         .addFields(
-            {name: '', value: 'https://discord.com/oauth2/authorize?client_id=827475563949326376&scope=bot&permissions=134590789'}
+            {name: 'Invite Link ', value: 'https://discord.com/oauth2/authorize?client_id=827475563949326376&scope=bot&permissions=134590789'}
        
         
 
