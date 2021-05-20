d1f8d2a5777c18a8d847fb92c22e1060dd1298f2
HorizonOP <79844761+HorizonOP@users.noreply.github.com>
2021-05-20 08:36:01 +0400

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,41 +1,35 @@
-const Discord = require('discord.js');
-const moment = require('moment');
-
-module.exports.run = async (bot, message, args) => {
-    let userArray = message.content.split(" ");
-    let userArgs = userArray.slice(1);
-    let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member;
-
-    if (member.presence.status === 'dnd') member.presence.status = 'Do Not Disturb';
-    if (member.presence.status === 'online') member.presence.status = 'Online';
-    if (member.presence.status === 'idle') member.presence.status = 'Idle';
-    if (member.presence.status === 'offline') member.presence.status = 'offline';
-
-    let x = Date.now() - member.createdAt;
-    let y = Date.now() - message.guild.members.cache.get(member.id).joinedAt;
-    const joined = Math.floor(y / 86400000);
-
-    const joineddate = moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY, HH:mm:ss");
-    let status = member.presence.status;
-
-    const userEmbed = new Discord.MessageEmbed()
-    .setAuthor(member.user.tag, member.user.displayAvatarURL())
-    .setTimestamp()
-    .setColor('BLUE')
-    .setImage(member.user.displayAvatarURL())
-    .addField("Member ID", member.id)
-    .addField('Roles', `<@&${member._roles.join('> <@&')}>`)
-    .addField("Account Created On:", ` ${moment.utc(member.user.createdAt).format("dddd, MMMM Do YYYY")}`, true) 
-    .addField('Joined the server At', `${joineddate} \n> ${joined} day(S) Ago`)
-    .addField("Status", status)
-
-    message.channel.send(userEmbed);
+const { MessageEmbed } = require("discord.js");
+const moment = require("moment")
+module.exports = {
+  name: "userinfo",
+  description: "Information about a user",
+  category: "fun",
+  run: async (Bot, msg, args) => {
+	let user;
+if (msg.mentions.users.first()) {
+    user = msg.mentions.users.first();
+} else {
+    user = msg.author;
 }
+var member = user;
 
-module.exports.config = {
-    name: "userinfo",
-    description: "Shows the information of a member/user",
-    usage: "?memberinfo",
-    accessableby: "Members",
-    aliases: ['userinfo']
-}
+
+    const embeduserinfo = new MessageEmbed()
+    .setColor("RANDOM")
+    .setThumbnail(msg.author.avatarURL)
+    .addField(`${user.tag}`, `${user}`, true)
+    .addField("ID:", `${user.id}`, true)
+    .addField("Permissions:", `${user.permissions}`, true)
+    .addField("Nickname:", `${user.id.displayName}`, true)
+    .addField("Status:", `${user.presence.status}`, true)
+    .addField("In Server", msg.guild.name, true)
+    .addField("Game:", `${user.presence.game ? user.presence.game.name : 'None'}`, true)
+    .addField("Bot:", `${user.bot}`, true)
+    .addField("Joined The Server On:", `${moment.utc(user.joinedAt).format("dddd, MMMM Do YYYY")}`, true)
+    .addField("Account Created On:", `${moment.utc(user.createdAt).format("dddd, MMMM Do YYYY")}`, true) 
+    .setFooter(`Replying to ${msg.author.username}#${msg.author.discriminator}`)
+	msg.channel.send(embeduserinfo)
+
+  }
+  
+};
