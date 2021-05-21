fe6d83f091e96ffe773fc13428eb47bc28856458
HorizonOP <79844761+HorizonOP@users.noreply.github.com>
2021-05-21 17:05:19 +0400

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -6,29 +6,29 @@ module.exports = {
   category: "fun",
   execute: async (message, args, Discord) => {
 	let user;
-if (msg.mentions.users.first()) {
-    user = msg.mentions.users.first();
+if (message.mentions.users.first()) {
+    user = message.mentions.users.first();
 } else {
-    user = msg.author;
+    user = message.author;
 }
 var member = user;
 
 
     const embeduserinfo = new MessageEmbed()
     .setColor("RANDOM")
-    .setThumbnail(msg.author.avatarURL)
+    .setThumbnail(message.author.avatarURL)
     .addField(`${user.tag}`, `${user}`, true)
     .addField("ID:", `${user.id}`, true)
     .addField("Permissions:", `${user.permissions}`, true)
     .addField("Nickname:", `${user.id.displayName}`, true)
     .addField("Status:", `${user.presence.status}`, true)
-    .addField("In Server", msg.guild.name, true)
+    .addField("In Server", message.guild.name, true)
     .addField("Game:", `${user.presence.game ? user.presence.game.name : 'None'}`, true)
     .addField("Bot:", `${user.bot}`, true)
     .addField("Joined The Server On:", `${moment.utc(user.joinedAt).format("dddd, MMMM Do YYYY")}`, true)
     .addField("Account Created On:", `${moment.utc(user.createdAt).format("dddd, MMMM Do YYYY")}`, true) 
-    .setFooter(`Replying to ${msg.author.username}#${msg.author.discriminator}`)
-	msg.channel.send(embeduserinfo)
+    .setFooter(`Replying to ${message.author.username}#${message.author.discriminator}`)
+	message.channel.send(embeduserinfo)
 
   }
   
