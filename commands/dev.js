7455820860e5ff7ad86a9b56bcc5654473b64a38
HorizonOP <79844761+HorizonOP@users.noreply.github.com>
2021-07-13 13:52:23 +0400

    Update dev.js

--- commands/dev.js
+++ commands/dev.js
@@ -8,7 +8,7 @@ module.exports = {
         .setURL('https://www.youtube.com/channel/UCJtbyCHmt_TAa9RxgC2aA8w')
         .setDescription('Developer and Support Server')
         .addFields(
-            {name: 'Developer and Owner of Horizon Nation', value: 'Horizon#7038'},
+            {name: 'Developer and Owner of Horizon Nation', value: 'HorizonX#0001'},
             {name: 'Join Horizon Nation Support/Dev Server', value: 'https://discord.gg/4n3mPA2u8v'}
        
         
